cd39d10125b5aafcb789f69b9e9cdeca1497db15
Nickael <qaznickael@protonmail.com>
2025-08-25 13:38:12 -0300

    change styles

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <template>
-  <div style="display: flex; height: 100vh; align-items: center; justify-content: center">
+  <div style="display: flex; height: 100vh; align-items: center; justify-content: center; background-color: #0A0A0A">
     <NuxtRouteAnnouncer />
-    <h1 style="text-align: center; ">Em Breve</h1>
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center">
+      <img src="/ketch-ai.svg" alt="Logo" style="width: 200px; height: 200px" />
+      <h1 style="text-align: center; margin-top: 20px; color: #D4D4D4">Em Breve</h1>
+    </div>
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Inter', sans-serif;
+}
+</style>
