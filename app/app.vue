164cf09d1f195e93ccafbff0fe6a46949f437a5d
Nickael <qaznickael@protonmail.com>
2025-08-25 09:44:35 -0300

    first commit

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="display: flex; height: 100vh; align-items: center; justify-content: center">
+    <NuxtRouteAnnouncer />
+    <h1 style="text-align: center; ">Em Breve</h1>
+  </div>
+</template>
